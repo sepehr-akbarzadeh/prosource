1868e68659557dcdd0a9c606ff0a54a640ae0ec0
sepehr.ak <sa.goldeneagle@gmail.com>
2020-06-13 21:36:08 -0700

    project prosource done

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".navbar").sticky({ topSpacing: 0 });
+});
+
+$recaptcha = $_POST["g-recaptcha-response"];
+$res = reCaptcha($recaptcha);
+if (!$res["success"]) {
+  // Error
+}
+function reCaptcha($recaptcha){
+  $secret = "6LctWqQZAAAAAOm-Np9eRJfRwFt8U3Tvk9XEyZcH";
+  $ip = $_SERVER['REMOTE_ADDR'];
+ 
+  $postvars = array("secret" => $secret, "response" => $recaptcha, "remoteip" => $ip);
+  $url = "https://www.google.com/recaptcha/api/siteverify";
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+  $data = curl_exec($ch);
+  curl_close($ch);
+ 
+  return json_decode($data, true);
+}
